fbedcb0e3bf671b617071dc25a2d4863ad758516
blekso <blekepix@outlook.com>
2023-07-24 23:09:29 +0200

    added profile page, finished authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,53 @@
-import axios from "axios";
+'use client';
+
 import React from "react";
-import { cookies } from 'next/headers'; // Import cookies
-import jwt_decode from "jwt-decode";
 import { LandingAuthPageInvisible } from "@/app/lib/protect-landing-page";
+import Image from 'next/image'
 
 
-const getUser = async () => {
+function handleClick() {
+  window.location.href = "https://www.ferit.unios.hr/new-login/?r=https://www.ferit.unios.hr/e-znanstveni-radovi/api/v1/api_jwt.php/login-jwt?path=http://localhost:3001/api/auth/handle-redirect";
+}
 
-  const nextCookies = cookies();
+export default function Login() {
+   return (
+   <>
+    <main className="h-screen flex items-center justify-center">
+        <div className="p-10 bg-gray-200 rounded-md shadow-lg">
+            {/* <!--Logo--> */}
+            <div className="text-center">
+              <Image
+                src="/ferit-logo.png"
+                width={259}
+                height={259}
+                alt="Picture of the author"
+              />
+              <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
+                Student reward system
+              </h4>
+            </div>
 
-  const token = nextCookies.get('accessToken')
+            <form>
+              <p className="mb-4">Prijavi se sa svojim AAI@EduHr računom</p>
 
-  console.log(token)
-
-  if(token) {
-    return jwt_decode(token.value);
-  }
-
-  /* try {
-    const response = await axios.get("/user", {
-      headers: {
-        // NOTE: If using NextJS < 13.4.0 use:
-        // authorization: `bearer ${token}` // Use your cookie
-        authorization: `bearer ${token.value}` // Use your cookie
-      }
-    })
-
-    return response.data.user;
-
-   } catch (err: any) {
-     console.log(token);
-     console.log(err.response.data);
-     return null;
-   } */
-};
-
-export default async function Login() {
-
-  const user: any = await getUser();
-
-  if (!user) return <div className="text-red-500">No user found</div>;
-
-  return <div>Logged in User:{JSON.stringify(user.ime)}</div>;
+              {/* <!--Submit button--> */}
+              <div className="mb-12 pb-1 pt-1 text-center">
+                  <button
+                    onClick={handleClick}
+                    className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                    type="button"
+                    style={{
+                      background:
+                        "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                    }}
+                  >
+                    prijava
+                  </button>
+              </div>
+            </form>
+        </div>
+      </main>
+      <LandingAuthPageInvisible />
+    </>
+  );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,20 +14,20 @@ export default function items() {
    return (
     <>
       <main className="flex min-h-screen flex-col items-center p-24 font-mono">
-      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
-          <div className="grid grid-cols-2 gap-8">
+      <div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
+        <div className="grid grid-cols-2 gap-8">
             <Link href="items">
-                <h2 className={`mb-3 text-2xl font-semibold underline`}>
+                <h2 className={`mb-3 text-2xl font-semibold`}>
                   Dostupne nagrade
                 </h2>
               </Link>
               <Link href="profile">
-                <h2 className={`mb-3 text-2xl font-semibold`}>
+                <h2 className={`mb-3 text-2xl font-semibold underline`}>
                   Moj profil
                 </h2>
               </Link>
           </div>
-          
+
           <button
             onClick={handleClick}
             className="mb-3 w-32 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
@@ -41,35 +41,21 @@ export default function items() {
           </button>
       </div>
 
-      <div className="mt-24 grid text-center lg:mb-0 lg:grid-cols-2 gap-8 lg:text-left">
-        <Link
-          href="profile"
-          className="grid grid-cols-2 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <div className="flex items-center justify-center">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-              src="/next.svg"
-              alt="Next.js Logo"
-              width={120}
-              height={120}
-              priority
-            />
-          </div>
-          
-          <div>
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-            Nagrada 1{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      <div className="mt-24 grid text-center lg:mb-0 lg:grid-cols-2 gap-24 lg:text-left">
+        <div>
+            <h3 className="mb-3 text-xl">Preuzete nagrade</h3>
+            <p>Nagrada 1</p>
+            <p>Nagrada 1</p>
+            <p>Nagrada 1</p>
+            <p>Nagrada 1</p>
+            <p>Nagrada 1</p>
         </div>
-        </Link>
+        <div>
+            <h3 className="mb-3 text-xl">Moj uspjeh</h3>
+            <p>Nagrada 1</p>
+        </div>
       </div>
+
     </main>
     <AuthPageInvisible />
     </>
